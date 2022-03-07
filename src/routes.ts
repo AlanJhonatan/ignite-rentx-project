22a2540bb7b0d4aa07fc48b7be71ff9572c2a2c7
AlanJhonatan <alan.amorim.franca@gmail.com>
2022-03-07 20:59:14 -0300

    lint: linted script

--- src/routes.ts
+++ src/routes.ts
@@ -1,17 +1,18 @@
 import { Request, Response } from 'express';
+
 import CreateCourseService from './CreateCourseService';
 
 export function createCourse(request: Request, response: Response) {
   CreateCourseService.execute({
     educator: 'Dani',
-    name: "nodejs",
+    name: 'nodejs',
     duration: 10,
   });
 
   CreateCourseService.execute({
     educator: 'Diego',
-    name: "React JS",
+    name: 'React JS',
   });
-  
+
   response.send();
 }
