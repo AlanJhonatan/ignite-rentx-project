60066afcf19fcf89312eca2ca083e21edf746388
AlanJhonatan <alan.amorim.franca@gmail.com>
2022-03-09 22:24:34 -0300

    refactor: using Singleton Pattern to keep same Repository Instance

--- src/modules/cars/repositories/implementations/SpecificationsRepository.ts
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -4,8 +4,18 @@ import { ICreateSpecificationDTO, ISpecificationsRepository } from '../ISpecific
 class SpecificationsRepository implements ISpecificationsRepository {
   private specifications: Specification[];
 
-  constructor() {
+  public static INSTANCE = new SpecificationsRepository();
+
+  private constructor() {
     this.specifications = [];
+  }
+
+  public static getInstance() {
+    if (!SpecificationsRepository.INSTANCE) {
+      SpecificationsRepository.INSTANCE = new SpecificationsRepository();
+    }
+
+    return SpecificationsRepository.INSTANCE;
   }
 
   findByName(name: string): Specification {
