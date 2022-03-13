662541f5152fa83c08cdac437d5a53c5dd1d091b
AlanJhonatan <alan.amorim.franca@gmail.com>
2022-03-13 12:27:33 -0300

    refactor(Categories): using database implementation

--- src/modules/cars/useCases/importCategory/index.ts
+++ src/modules/cars/useCases/importCategory/index.ts
@@ -1,8 +1,7 @@
-import { CategoriesRepository } from '../../repositories/implementations/CategoriesRepository';
 import { ImportCategoryController } from './ImportCategoryController';
 import { ImportCategoryUseCase } from './ImportCategoryUseCase';
 
-const categoryRepository = CategoriesRepository.getInstance();
+const categoryRepository = null; // new CategoriesRepository();
 const importCategoryUseCase = new ImportCategoryUseCase(categoryRepository);
 const importCategory = new ImportCategoryController(importCategoryUseCase);
 
