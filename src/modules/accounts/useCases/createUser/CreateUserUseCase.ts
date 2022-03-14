41fc8ca0c43a4cf5ed7fea8372167628a464b0bf
AlanJhonatan <alan.amorim.franca@gmail.com>
2022-03-14 11:49:40 -0300

    chore(createUser): hashing user password to save in database

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,3 +1,4 @@
+import { hash } from 'bcrypt';
 import { inject, injectable } from 'tsyringe';
 
 import { ICreateUserDTO } from '../../dtos/ICreateUserDTO';
@@ -13,8 +14,16 @@ class CreateUserUseCase {
   async execute({
     name, email, driver_license, password,
   }: ICreateUserDTO): Promise<void> {
+    const userAlreadyExists = await this.usersRepository.findByEmail(email);
+
+    if (userAlreadyExists) {
+      throw new Error('This email has already taken !');
+    }
+
+    const passwordHash = await hash(password, 8);
+
     await this.usersRepository.create({
-      name, email, driver_license, password,
+      name, email, driver_license, password: passwordHash,
     });
   }
 }
