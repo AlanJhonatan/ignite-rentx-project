483fbe1414d250b5a263923426db02276411efb3
AlanJhonatan <alan.amorim.franca@gmail.com>
2022-03-14 12:40:05 -0300

    chore(ensureAuthentication): implemented middleware to check if token is valid and if user exists

--- src/routes/specification.routes.ts
+++ src/routes/specification.routes.ts
@@ -2,11 +2,14 @@ import { Router } from 'express';
 
 import { CreateSpecificationController } from '../modules/cars/useCases/createSpecification/CreateSpecificationController';
 import { ListSpecificationsController } from '../modules/cars/useCases/listSpecifications/ListSpecificationsController';
+import { ensureAuthenticated } from '../modules/middlewares/ensureAuthenticated';
 
 const specificationsRoutes = Router();
 
 const createSpecification = new CreateSpecificationController();
 const listSpecifications = new ListSpecificationsController();
+
+specificationsRoutes.use(ensureAuthenticated);
 
 specificationsRoutes.get('/', listSpecifications.handle);
 specificationsRoutes.post('/', createSpecification.handle);
