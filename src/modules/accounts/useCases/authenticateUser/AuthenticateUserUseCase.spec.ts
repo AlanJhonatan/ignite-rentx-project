5463310b3ccb4a85466bf30ee379a930ad44b47e
AlanJhonatan <alan.amorim.franca@gmail.com>
2022-03-15 13:40:03 -0300

    tests(AuthenticateUser): created tests to user authentication route

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -0,0 +1,59 @@
+import { AppError } from '../../../../errors/AppError';
+import { ICreateUserDTO } from '../../dtos/ICreateUserDTO';
+import { UsersRepositoryInMemory } from '../../repositories/in-memory/UsersRepositoryInMemory';
+import { CreateUserUseCase } from '../createUser/CreateUserUseCase';
+import { AuthenticateUserUseCase } from './AuthenticateUserUseCase';
+
+let authenticateUserUseCase: AuthenticateUserUseCase;
+let usersRepositoryInMemory: UsersRepositoryInMemory;
+let createUserUseCase: CreateUserUseCase;
+
+describe('Authenticate User', () => {
+  beforeEach(() => {
+    usersRepositoryInMemory = new UsersRepositoryInMemory();
+    createUserUseCase = new CreateUserUseCase(usersRepositoryInMemory);
+    authenticateUserUseCase = new AuthenticateUserUseCase(usersRepositoryInMemory);
+  });
+
+  it('should be able to authenticate an user', async () => {
+    const user: ICreateUserDTO = {
+      driver_license: '0000123',
+      email: 'user@test.com',
+      password: 'password',
+      name: 'test',
+    };
+
+    await createUserUseCase.execute(user);
+
+    const result = await authenticateUserUseCase.execute({
+      email: user.email,
+      password: user.password,
+    });
+
+    expect(result).toHaveProperty('token');
+  });
+
+  it('should not be able to authenticate an non existent user', async () => {
+    expect(async () => {
+      await authenticateUserUseCase.execute({ email: 'non.existent@mail.com', password: 'fail' });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to authenticate an user with incorrect password', async () => {
+    expect(async () => {
+      const user: ICreateUserDTO = {
+        driver_license: '0000123',
+        email: 'user@test.com',
+        password: 'password',
+        name: 'test',
+      };
+
+      await createUserUseCase.execute(user);
+
+      await authenticateUserUseCase.execute({
+        email: user.email,
+        password: 'wrongPassword',
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+});
