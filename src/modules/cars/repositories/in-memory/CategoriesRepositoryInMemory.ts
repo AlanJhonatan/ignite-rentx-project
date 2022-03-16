4a7739e090b3e99757538f9e8e1c132146c38c4c
AlanJhonatan <alan.amorim.franca@gmail.com>
2022-03-16 07:47:41 -0300

    fix: imports using @

--- src/modules/cars/repositories/in-memory/CategoriesRepositoryInMemory.ts
+++ src/modules/cars/repositories/in-memory/CategoriesRepositoryInMemory.ts
@@ -1,12 +1,9 @@
-import { Category } from '../../infra/typeorm/entities/Category';
+import { Category } from '@modules/cars/infra/typeorm/entities/Category';
+
 import { ICategoriesRepository, ICreateCategoryDTO } from '../ICategoriesRepository';
 
 class CategoriesRepositoryInMemory implements ICategoriesRepository {
   categories: Category[] = [];
-
-  // constructor() {
-  //   this.categories = [];
-  // }
 
   async findByName(name: string): Promise<Category> {
     const category = this.categories.find((category) => category.name === name);
