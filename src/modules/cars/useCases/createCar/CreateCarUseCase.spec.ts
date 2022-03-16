c029b02ecc9a21cc88f9e86e7beb5569477c84b8
AlanJhonatan <alan.amorim.franca@gmail.com>
2022-03-16 09:12:14 -0300

    test(createCar): testing basic car creation

---
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
@@ -0,0 +1,26 @@
+import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
+import { CarsRepositoryInMemory } from '@modules/cars/repositories/in-memory/CarsRepositoryInMemory';
+
+import { CreateCarUseCase } from './CreateCarUseCase';
+
+let carsRepositoryInMemory: ICarsRepository;
+let createCarUseCase: CreateCarUseCase;
+
+describe('Create Car', () => {
+  beforeEach(() => {
+    carsRepositoryInMemory = new CarsRepositoryInMemory();
+    createCarUseCase = new CreateCarUseCase(carsRepositoryInMemory);
+  });
+
+  it('should be able to create a new car', async () => {
+    await createCarUseCase.execute({
+      name: 'Car Name',
+      description: 'Car Description',
+      daily_rate: 100,
+      license_plate: 'ABC-1234',
+      fine_amount: 60,
+      brand: 'car brand',
+      category_id: 'category',
+    });
+  });
+});
