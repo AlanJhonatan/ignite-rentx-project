9bac433fa1fcb8bd790b2e79757cb17d8a74e2c6
AlanJhonatan <alan.amorim.franca@gmail.com>
2022-03-16 11:56:19 -0300

    chore(Car): changed entity to handle default creation infos

--- src/modules/cars/infra/typeorm/entities/Car.ts
+++ src/modules/cars/infra/typeorm/entities/Car.ts
@@ -1,14 +1,53 @@
+import {
+  Column, CreateDateColumn, Entity, JoinColumn, PrimaryColumn,
+} from 'typeorm';
+import { v4 as uuid } from 'uuid';
+
+import { Category } from './Category';
+
+@Entity('cars')
 class Car {
-  id: string;
-  name: string;
-  description: string;
-  daily_rate: number;
-  available: boolean;
-  license_plate: string;
-  fine_amount: number;
-  brand: string;
-  created_at: string;
-  category_id: string;
+  @PrimaryColumn()
+    id: string;
+
+  @Column()
+    name: string;
+
+  @Column()
+    description: string;
+
+  @Column()
+    daily_rate: number;
+
+  @Column()
+    available: boolean;
+
+  @Column()
+    license_plate: string;
+
+  @Column()
+    fine_amount: number;
+
+  @Column()
+    brand: string;
+
+  @CreateDateColumn()
+    created_at: Date;
+
+  // many to one relationship.
+  // many cars to an single category
+  @JoinColumn({ name: 'category_id' })
+    category: Category;
+
+  @Column()
+    category_id: string;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+      this.available = true;
+    }
+  }
 }
 
 export { Car };
