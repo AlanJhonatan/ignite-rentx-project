c3d9449bf7fa4fc11856b390d7b4b237636c4d07
AlanJhonatan <alan.amorim.franca@gmail.com>
2022-03-16 23:11:52 -0300

    chore(ListAvailableCars): implemented controller

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -8,7 +8,9 @@ import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 const carsRoutes = Router();
 
 const createCarController = new CreateCarController();
+const listAvailableCarsController = new ListAvailableCarsController();
 
 carsRoutes.post('/', ensureAuthenticated, ensureAdmin, createCarController.handle);
+carsRoutes.get('/available', listAvailableCarsController.handle);
 
 export { carsRoutes };
