1ce45c4908111ff7f6efc0272a92db08b8899dd1
AlanJhonatan <alan.amorim.franca@gmail.com>
2022-03-18 08:19:34 -0300

    chore(RentalRepository): created Repository interface and DTO

---
+++ src/modules/rentals/repositories/IRentalsRepository.ts
@@ -0,0 +1,14 @@
+import { ICreateRentalDTO } from '../dtos/ICreateRentalDTO';
+import { Rental } from '../infra/typeorm/entities/Rental';
+
+interface IRentalsRepository {
+  create({
+    user_id,
+    car_id,
+    expect_return_date,
+  }: ICreateRentalDTO): Promise<Rental>;
+  findOpenRentalByCarId(car_id: string): Promise<Rental>;
+  findOpenRentalByUserId(user_id: string): Promise<Rental>;
+}
+
+export { IRentalsRepository };
