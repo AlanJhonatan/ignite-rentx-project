83d32afbcdb60a2069818a4bfac201531a845743
AlanJhonatan <alan.amorim.franca@gmail.com>
2022-03-23 09:09:23 -0300

    test: fixed unit testing using new business rule for car availability

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
@@ -1,3 +1,4 @@
+import { CarsRepositoryInMemory } from '@modules/cars/repositories/in-memory/CarsRepositoryInMemory';
 import { RentalsRepositoryInMemory } from '@modules/rentals/repositories/in-memory/RentalsRepositoryInMemory';
 import dayjs from 'dayjs';
 
@@ -7,19 +8,33 @@ import { AppError } from '@shared/errors/AppError';
 import { CreateRentalUseCase } from './CreateRentalUseCase';
 
 let rentalsRepositoryInMemory: RentalsRepositoryInMemory;
+let carsRepositoryInMemory: CarsRepositoryInMemory;
 let createRentalUseCase: CreateRentalUseCase;
 let dayjsDateProvider: DayjsDateProvider;
 
 describe('Create Rental', () => {
   const dayAdd24Hours = dayjs().add(1, 'day').toDate();
 
-  beforeEach(() => {
+  beforeEach(async () => {
     rentalsRepositoryInMemory = new RentalsRepositoryInMemory();
+    carsRepositoryInMemory = new CarsRepositoryInMemory();
     dayjsDateProvider = new DayjsDateProvider();
     createRentalUseCase = new CreateRentalUseCase(
       rentalsRepositoryInMemory,
-      dayjsDateProvider
+      dayjsDateProvider,
+      carsRepositoryInMemory
     );
+
+    await carsRepositoryInMemory.create({
+      id: 'car-1111',
+      brand: 'Honda',
+      category_id: '1',
+      name: 'Civic',
+      license_plate: 'ABC-1234',
+      description: 'Civic',
+      daily_rate: 100,
+      fine_amount: 100,
+    });
   });
 
   it('should be able to create an new rental', async () => {
