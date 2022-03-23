2980fe241523a4d018df6f4d70e5fc00c8db6bef
AlanJhonatan <alan.amorim.franca@gmail.com>
2022-03-23 16:24:44 -0300

    chore(UserTokensRepository): implemented postgres and in memo repository

---
+++ src/modules/accounts/repositories/in-memory/UsersTokensRepositoryInMemory.ts
@@ -0,0 +1,29 @@
+import { ICreateUserTokenDTO } from '@modules/accounts/dtos/ICreateUserTokenDTO';
+import { UserTokens } from '@modules/accounts/infra/typeorm/entities/UserTokens';
+
+import { IUsersTokensRepository } from '../IUsersTokensRepository';
+
+class UsersTokensRepositoryInMemory implements IUsersTokensRepository {
+  private usersTokens: UserTokens[] = [];
+
+  async create({
+    expires_date,
+    refresh_token,
+    user_id,
+  }: ICreateUserTokenDTO): Promise<UserTokens> {
+    const userToken = new UserTokens();
+
+    Object.assign(userToken, {
+      expires_date,
+      refresh_token,
+      user_id,
+    });
+
+    this.usersTokens.push(userToken);
+
+    // return Promise.resolve(userToken);
+    return userToken;
+  }
+}
+
+export { UsersTokensRepositoryInMemory };
