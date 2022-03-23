2d61a2fba8b7bd608ae4a76f9b085b5770e15eb5
AlanJhonatan <alan.amorim.franca@gmail.com>
2022-03-23 13:06:38 -0300

    chore(ListRentalsByUserController): implemented in rentalRoutes and changed ensureAuthenticated to handle before all controllers

--- src/shared/infra/http/routes/rental.routes.ts
+++ src/shared/infra/http/routes/rental.routes.ts
@@ -1,19 +1,20 @@
 import { CreateRentalController } from '@modules/rentals/useCases/createRental/CreateRentalController';
 import { DevolutionRentalController } from '@modules/rentals/useCases/devolutionRental/DevolutionRentalController';
+import { ListRentalsByUserController } from '@modules/rentals/useCases/listRentalsByUser/ListRentalsByUserController';
 import { Router } from 'express';
 
+import { app } from '../app';
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
 
 const rentalRoutes = Router();
 
 const createRentalController = new CreateRentalController();
 const devolutionRentalController = new DevolutionRentalController();
+const listRentalsByUserController = new ListRentalsByUserController();
 
-rentalRoutes.post('/', ensureAuthenticated, createRentalController.handle);
-rentalRoutes.post(
-  '/devolution/:id',
-  ensureAuthenticated,
-  devolutionRentalController.handle
-);
+rentalRoutes.use(ensureAuthenticated);
+rentalRoutes.post('/', createRentalController.handle);
+rentalRoutes.post('/devolution/:id', devolutionRentalController.handle);
+rentalRoutes.get('/user', listRentalsByUserController.handle);
 
 export { rentalRoutes };
