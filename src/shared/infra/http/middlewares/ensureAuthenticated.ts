320f21668fa3e79c721dfcb7823cd847bb7ee200
AlanJhonatan <alan.amorim.franca@gmail.com>
2022-03-24 08:44:29 -0300

    refactor: using refresh token instead token to validate trough routes

--- src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -1,4 +1,6 @@
+import auth from '@config/auth';
 import { UsersRepository } from '@modules/accounts/infra/typeorm/repositories/UsersRepository';
+import { UsersTokensRepository } from '@modules/accounts/infra/typeorm/repositories/UsersTokensRepository';
 import { NextFunction, Request, Response } from 'express';
 import { verify } from 'jsonwebtoken';
 
@@ -11,9 +13,11 @@ interface IPayload {
 export async function ensureAuthenticated(
   request: Request,
   response: Response,
-  next: NextFunction,
+  next: NextFunction
 ) {
   const authHeader = request.headers.authorization;
+
+  const userTokenRepository = new UsersTokensRepository();
 
   if (!authHeader) {
     throw new AppError('Token is missing.', 401);
@@ -22,11 +26,15 @@ export async function ensureAuthenticated(
   const [, token] = authHeader.split(' ');
 
   try {
-    const { sub: user_id } = verify(token, '784909df10d6d1e339983aacfbe82ea5') as IPayload;
+    const { sub: user_id } = verify(
+      token,
+      auth.secret_refresh_token
+    ) as IPayload;
 
-    const usersRepository = new UsersRepository();
-
-    const user = await usersRepository.findById(user_id);
+    const user = await userTokenRepository.findByUserIdAndRefreshToken(
+      user_id,
+      token
+    );
 
     if (!user) {
       throw new AppError('This user does not exist', 401);
