d88178e10e98861f39c660d81813cf7cbd88c796
AlanJhonatan <alan.amorim.franca@gmail.com>
2022-03-24 08:34:28 -0300

    chore(jwt): created default config file to save secrets for token and refresh_token

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: '784909df10d6d1e339983aacfbe82ea5',
+  secret_refresh_token: '125c41faf66e2c351bff02def3b22cab',
+  expires_in_token: '15m',
+  expires_in_refresh_token: '30d',
+  expires_refresh_token_days: 30,
+};
