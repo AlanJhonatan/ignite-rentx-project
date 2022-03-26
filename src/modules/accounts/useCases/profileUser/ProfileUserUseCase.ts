9c6544ba32a39a184a492fa813857c616825026c
AlanJhonatan <alan.amorim.franca@gmail.com>
2022-03-26 18:41:55 -0300

    chore(ProfileUser): implemented user profile route

---
+++ src/modules/accounts/useCases/profileUser/ProfileUserUseCase.ts
@@ -0,0 +1,19 @@
+import { User } from '@modules/accounts/infra/typeorm/entities/User';
+import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository';
+import { inject, injectable } from 'tsyringe';
+
+@injectable()
+class ProfileUserUseCase {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository
+  ) {}
+
+  async execute(user_id: string): Promise<User> {
+    const userProfile = await this.usersRepository.findById(user_id);
+
+    return userProfile;
+  }
+}
+
+export { ProfileUserUseCase };
--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -1,5 +1,6 @@
 import uploadConfig from '@config/upload';
 import { CreateUserController } from '@modules/accounts/useCases/createUser/CreateUserController';
+import { ProfileUserController } from '@modules/accounts/useCases/profileUser/ProfileUserController';
 import { UpdateUserAvatarController } from '@modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController';
 import { Router } from 'express';
 import multer from 'multer';
@@ -12,8 +13,10 @@ const uploadAvatar = multer(uploadConfig);
 
 const createUserController = new CreateUserController();
 const updateUserAvatarController = new UpdateUserAvatarController();
+const profileUserController = new ProfileUserController();
 
 usersRoutes.post('/', createUserController.handle);
+usersRoutes.get('/profile', ensureAuthenticated, profileUserController.handle);
 usersRoutes.patch(
   '/avatar',
   ensureAuthenticated,
