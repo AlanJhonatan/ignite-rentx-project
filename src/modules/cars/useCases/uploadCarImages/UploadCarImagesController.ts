b30962f12b2bf895298310648e0f2a056fac42f0
AlanJhonatan <alan.amorim.franca@gmail.com>
2022-03-26 18:02:07 -0300

    refactor: using Storage Provider implementation

--- src/modules/cars/useCases/uploadCarImages/UploadCarImagesController.ts
+++ src/modules/cars/useCases/uploadCarImages/UploadCarImagesController.ts
@@ -15,12 +15,13 @@ class UploadCarImagesController {
     const uploadCarImagesUseCase = container.resolve(UploadCarImagesUseCase);
 
     const images_name = images.map((file) => file.filename);
-    const carsImages = await uploadCarImagesUseCase.execute({
+
+    await uploadCarImagesUseCase.execute({
       car_id: id,
       images_name,
     });
 
-    return response.status(201).json(carsImages);
+    return response.status(201).send();
   }
 }
 
--- src/modules/cars/useCases/uploadCarImages/UploadCarImagesUseCase.ts
+++ src/modules/cars/useCases/uploadCarImages/UploadCarImagesUseCase.ts
@@ -2,6 +2,8 @@ import { CarImage } from '@modules/cars/infra/typeorm/entities/CarImage';
 import { CarsImagesRepository } from '@modules/cars/repositories/in-memory/CarsImagesRepository';
 import { inject, injectable } from 'tsyringe';
 
+import { IStorageProvider } from '@shared/container/providers/StorageProvider/IStorageProvider';
+
 interface IRequest {
   car_id: string;
   images_name: string[];
@@ -11,22 +13,17 @@ interface IRequest {
 class UploadCarImagesUseCase {
   constructor(
     @inject('CarsImagesRepository')
-    private carsImagesRepository: CarsImagesRepository
+    private carsImagesRepository: CarsImagesRepository,
+
+    @inject('StorageProvider')
+    private storageProvider: IStorageProvider
   ) {}
 
-  async execute({ car_id, images_name }: IRequest): Promise<CarImage[]> {
-    const carsImages = images_name.map(async (image_name) => {
-      const carImage = await this.carsImagesRepository.create(
-        car_id,
-        image_name
-      );
-
-      return carImage;
+  async execute({ car_id, images_name }: IRequest): Promise<void> {
+    images_name.map(async (image) => {
+      await this.carsImagesRepository.create(car_id, image);
+      await this.storageProvider.save(image, 'cars');
     });
-
-    const createdCarsImages = await Promise.all(carsImages);
-
-    return createdCarsImages;
   }
 }
 
