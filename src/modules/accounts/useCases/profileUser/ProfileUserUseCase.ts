d1e88d7eaa3e365c3fafcece04c262d9ad7fbf75
AlanJhonatan <alan.amorim.franca@gmail.com>
2022-03-26 21:36:56 -0300

    chore: using avatar link instead of filename and enable express static files handling

--- src/modules/accounts/useCases/profileUser/ProfileUserUseCase.ts
+++ src/modules/accounts/useCases/profileUser/ProfileUserUseCase.ts
@@ -1,4 +1,5 @@
-import { User } from '@modules/accounts/infra/typeorm/entities/User';
+import { IUserResponseDTO } from '@modules/accounts/dtos/IUserResponseDTO';
+import { UserMap } from '@modules/accounts/mapper/UserMap';
 import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository';
 import { inject, injectable } from 'tsyringe';
 
@@ -9,10 +10,10 @@ class ProfileUserUseCase {
     private usersRepository: IUsersRepository
   ) {}
 
-  async execute(user_id: string): Promise<User> {
+  async execute(user_id: string): Promise<IUserResponseDTO> {
     const userProfile = await this.usersRepository.findById(user_id);
 
-    return userProfile;
+    return UserMap.toDTO(userProfile);
   }
 }
 
